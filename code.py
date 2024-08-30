76e85106ba2ace1310e6d78e222f69ee147e7b8e
ziad0taai <ziadtaai@gmail.com>
2024-08-30 23:57:30 +0100

    Add files via upload

---
+++ code.py
@@ -0,0 +1,4 @@
+lenght = float(input("entre the lenght of the triangle:" ))
+width = float(input("entre the width of the triangle:" ))
+area = lenght * width
+print(f"the area is: {area} cm^2") # type: ignore
